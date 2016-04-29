cf18f621889d91c6795e6520431f393ac7304d15
John Sullivan <jsullivan@csumb.edu>
2016-04-29 00:03:08 -0700

    Better Documentation

--- dtwread.cs
+++ dtwread.cs
@@ -1,3 +1,15 @@
+//dtwread
+//
+//Usage:
+//dtwread {training_data} {test_data} data_column {opt: match_window}
+//
+//Arguments:
+//
+//training_data/test_data: two 4 field CSVs without titles, assumed format of values x,y,z,time
+//data_column: which column of the data to test
+//match_window (Optional): set a match window distance for which to map points, must be larger
+//than the absolute difference of your input/training sets' size or score will be -1 (Null)
+
 using System;
 using System.IO;
 using System.Collections.Generic;
